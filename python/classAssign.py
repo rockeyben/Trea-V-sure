5ad32dfec18672419d1abf3baa3057a4c1602430
wzdlc1996 <wzdlc1996@gmail.com>
2019-12-26 17:27:52 +0800

    add classAssign.py for main loop and mx's data

--- python/classAssign.py
+++ python/classAssign.py
@@ -1,13 +1,30 @@
 # -*- coding: utf-8 -*-
 
-goodName = []
-tradName = []
+import json
 
-with open("../goodName.txt", "r") as f:
+with open("./cls.json", "r") as f:
+    clsInfo = json.load(f)
+
+clsVals = [z for x in clsInfo.values() for z in x]
+oldone = 0
+newone = 0
+goodName = []
+
+with open("../test.txt", "r") as f:
     for x in f.readlines():
-        goodName.append(x)
+        oldone += 1
+        check = True
+        for key in clsVals:
+            if key in x:
+                check = False
+        if check:
+            goodName.append(x)
+            newone += 1
+
+with open("../test.txt", "w") as f:
+    for x in goodName:
+        f.write(x)
         
-with open("../tradName.txt", "r") as f:
-    for x in f.readlines():
-        tradName.append(x)
-        
+print("Compress Ratio is: "+("%0.4f" % (newone/oldone)))
+    
+
--- python/dataPreHand.py
+++ python/dataPreHand.py
@@ -5,7 +5,7 @@ import csv
 useless = ["\"", " ", "\n", "\t"]
 
 data = []
-with open("../data.csv", "r") as f:
+with open("../data_sch.csv", "r") as f:
     lineN = 0
     csvd = csv.reader(f)
     for x in f.readlines():
@@ -15,6 +15,23 @@ with open("../data.csv", "r") as f:
                 x = x.replace(patt, "")
             keys = x.split(",")
         if lineN > 5 and lineN < 5316:
+            tempDict = {}
+            for patt in useless:
+                x = x.replace(patt, "")
+            for (key,val) in zip(keys, x.split(",")):
+                tempDict[key] = val
+            data.append(tempDict)
+            
+with open("../data_mx.csv", "r") as f:
+    lineN = 0
+    csvd = csv.reader(f)
+    for x in f.readlines():
+        lineN += 1
+        if lineN == 5:
+            for patt in useless:
+                x = x.replace(patt, "")
+            keys = x.split(",")
+        if lineN > 5 and lineN < 3627:
             tempDict = {}
             for patt in useless:
                 x = x.replace(patt, "")
